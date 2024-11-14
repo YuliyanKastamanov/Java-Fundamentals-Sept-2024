ad19e96131b48ba5b289a7e31f11f750834a3f76
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2024-11-14 17:33:55 +0200

    Associative Arrays - Lab

---
+++ 19-RegEx/MatchFullName_01.java
@@ -0,0 +1,31 @@
+package RegEx;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName_01 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = "\\b(?<firstName>[A-Z][a-z]+) (?<lastName>[A-Z][a-z]+)\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+            String currentName = matcher.group() + " ";
+
+            System.out.print(currentName);
+        }
+
+
+
+    }
+}
---
+++ 19-RegEx/MatchPhoneNumber_02.java
@@ -0,0 +1,35 @@
+package RegEx;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber_02 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String input = scanner.nextLine();
+
+        String regex = "\\+359([- ])2\\1[\\d]{3}\\1[\\d]{4}\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> matchedPhoneNumbers = new ArrayList<>();
+
+        while (matcher.find()){
+
+            matchedPhoneNumbers.add(matcher.group());
+        }
+
+        System.out.println(String.join(", ", matchedPhoneNumbers));
+
+
+    }
+}
